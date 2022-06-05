254cb353af9fcb3e5b0a7d08f2e52f698acd53da
jain.rr99@gmail.com <jain.rr99@gmail.com>
2022-06-05 21:31:11 +0530

    image cropper

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Cropper from './component/Cropper';
 
 function App() {
+  const [file,setSelectedFile]=useState<any>(null);
+  const [croppedImage,setCroppedimage]=useState<any>(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input
+      type="file"
+      accept="image/*"
+      onChange={(e:any) => {
+        setSelectedFile(e.target.files[0]);
+      }}
+    />
+    {/*to display the cropped output */}
+    {croppedImage && <img src={croppedImage} alt="uploaded"></img>}
+    {/* call the cropper */}
+    {file &&  <Cropper imageToCrop={URL.createObjectURL(file)} croppedImage={(value:any)=>setCroppedimage(value)}/> }
+    </>
   );
 }
 
